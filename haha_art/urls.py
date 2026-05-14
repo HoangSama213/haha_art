f01a42c897de8b08c977395a4ceaeb2d7b6e878b
Hoang <itc@gmail.com>
2026-05-14 12:19:38 +0700

    fix lat vat

--- haha_art/urls.py
+++ haha_art/urls.py
@@ -4,6 +4,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "IP System Admin"
+admin.site.site_title = "IP System"
+admin.site.index_title = "Quản trị hệ thống"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
